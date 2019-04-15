0743af09486fabef8ab1d4c5f285838bd1e436cb
mjarenyap <mjaren.yap@gmail.com>
2019-04-15 16:34:56 +0800

    Implemented active states for navigation

--- content/events.md
+++ content/events.md
@@ -3,5 +3,5 @@ title: "Events & Articles"
 date: 2019-02-17T20:13:32+08:00
 draft: false
 page: "pages/events"
-active: "2"
+active: "1"
 ---
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,1 +1,6 @@
 /* Navigation functionalities */
+function setActive(index){
+	var main_items = $('#main-nav ul li');
+	main_items.removeClass("active");
+	$(main_items[index]).addClass("active");
+}
